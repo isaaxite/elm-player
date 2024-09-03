5602ff431e7c885c23fd72a981cb12b68e89a7a4
isaaxite <isaaxite@gmail.com>
2024-09-03 18:12:01 +0800

    feat(build): update config
    - split babel conf to main and renderer
    - noemit in tsconfig
    - remove rollup-typescript plugin

--- rollup.main.config.mjs
+++ rollup.main.config.mjs
@@ -18,14 +18,10 @@ export default {
     }),
     commonjs(),
     babel({
-      babelHelpers: 'bundled', // 将 Babel 的 helper 函数打包进代码
-      presets: [
-        '@babel/preset-env', // 编译现代 JavaScript
-        // '@babel/preset-react', // 编译 React 代码
-        '@babel/preset-typescript' // 编译 TypeScript 代码
-      ],
-      extensions: ['.ts'], // 处理这些扩展名的文件
-      exclude: 'node_modules/**' // 排除 node_modules 目录
+      babelHelpers: 'bundled',
+      extensions: ['.ts'],
+      babelrc: false,
+      configFile: './.babelrc.main.json'
     })
   ]
 };
