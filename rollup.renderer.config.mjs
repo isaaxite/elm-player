6a6c607360ec31786e4d6b64c265417d8991ff3e
isaaxite <isaaxite@gmail.com>
2024-09-05 18:34:08 +0800

    feat(chore): remove unuse rollup-plugin

--- rollup.renderer.config.mjs
+++ rollup.renderer.config.mjs
@@ -3,7 +3,6 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import replace from '@rollup/plugin-replace';
 import postcss from 'rollup-plugin-postcss';
-import inject from '@rollup/plugin-inject';
 
 export default {
 	input: {
@@ -11,34 +10,8 @@ export default {
   },
 	output: {
     dir: 'dist/',
-		format: 'iife',
-    // globals: {
-    //   'global/window': 'window',
-    //   'global/document': 'document',
-    //   'safe-json-parse/tuple': 'safeJsonParseTuple',
-    //   '@videojs/vhs-utils/es/byte-helpers': 'byteHelpers',
-    //   '@videojs/vhs-utils/es/resolve-url': 'resolveUrl',
-    //   '@videojs/vhs-utils/es/media-groups': 'mediaGroups',
-    //   '@videojs/vhs-utils/es/decode-b64-to-uint8-array': 'decodeB64ToUint8Array',
-    //   'mux.js/lib/tools/parse-sidx': 'parseSidx',
-    //   '@videojs/vhs-utils/es/id3-helpers': 'id3Helpers',
-    //   '@videojs/vhs-utils/es/containers': 'containers',
-    //   'mux.js/lib/utils/clock': 'clock',
-    // }
-	},
-  // external: [
-  //   'global/window',
-  //   'global/document',
-  //   'safe-json-parse/tuple',
-  //   '@videojs/vhs-utils/es/byte-helpers',
-  //   '@videojs/vhs-utils/es/resolve-url',
-  //   '@videojs/vhs-utils/es/media-groups',
-  //   '@videojs/vhs-utils/es/decode-b64-to-uint8-array',
-  //   'mux.js/lib/tools/parse-sidx',
-  //   '@videojs/vhs-utils/es/id3-helpers',
-  //   '@videojs/vhs-utils/es/containers',
-  //   'mux.js/lib/utils/clock',
-  // ],
+		format: 'iife'
+  },
   plugins: [
     replace({
       'process.env.NODE_ENV': JSON.stringify('production'), // 自定义环境变量
@@ -58,10 +31,6 @@ export default {
       babelrc: false,
       configFile: './.babelrc.renderer.json',
       extensions: [".tsx", ".ts"]
-    }),
-    inject({
-      window123: 'global/window',       // 将 `window` 自动注入为 `global/window` 模块
-      include: 'src/renderer/index.ts'
-    }),
+    })
   ]
 };
